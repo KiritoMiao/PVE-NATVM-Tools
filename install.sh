3a51b90533869509660fdf7947840ee3dbab5f2d
kirito <i@ixh.me>
2021-11-23 14:44:26 -0700

    First Version

--- install.sh
+++ install.sh
@@ -17,6 +17,13 @@ read -p 'Input Private network CIDR: ' CIDR
 echo ""
 echo "Initial System..."
 echo ""
+echo 'auto '$VMBRNM >> /etc/network/interfaces
+echo 'iface '$VMBRNM' inet static' >> /etc/network/interfaces
+echo '	address  '$ADDRESS >> /etc/network/interfaces
+echo '	netmask  '$NETMASK >> /etc/network/interfaces
+echo '	bridge_ports none' >> /etc/network/interfaces
+echo '	bridge_stp off' >> /etc/network/interfaces
+echo '	bridge_fd 0' >> /etc/network/interfaces
 echo 'net.ipv4.ip_forward = 1' >> /etc/sysctl.conf
 sysctl -p
 iptables -t nat -A POSTROUTING -s $ADDRESS/$CIDR  -j MASQUERADE
@@ -26,9 +33,9 @@ iptables-save > /etc/iptables/rules.v4
 echo ""
 echo "Install Proxmox VE Nat Tools and all it dependents..."
 echo ""
-
+apt install python3 python3-pip python3-venv git -y
 git clone https://github.com/KiritoMiao/PVE-NATVM-Tools ./
-apt install python3 python3-pip python3-venv -y
+
 python3 -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
